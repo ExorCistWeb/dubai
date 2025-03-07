09e00eec386622d5faa074bdef5143294e902b38
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2025-03-07 23:01:19 +0700

    first commit

---
+++ js/swipper.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".servicesSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper_next",
+        prevEl: ".swiper_prev",
+    },
+});
+var swiper = new Swiper(".servicesVipSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper_next",
+        prevEl: ".swiper_prev",
+    },
+});
